9857b70be3eae7dced7f26a3025f97ea966138cc
Lucas Ribeiro <lucas.and.rib@gmail.com>
2024-01-05 18:40:51 -0300

    Adds NTP server data capture.

--- f5-load-balancer-monitor.cpp
+++ f5-load-balancer-monitor.cpp
@@ -1,7 +1,25 @@
+// Libraries import:
+#include <time.h>
+
 // Pinout:
 const int ldr_pin = 32;
 const int mq_pin = 33;
 const int dht_pin = 4;
+
+// Local date and time capture and time zone correction:
+#define NTP_SERVER "pool.ntp.br"
+const int daylight_offset_sec = 0;
+const long gmt_offset_sec = -3600*3;
+
+// Global variables:
+const int debug = 2;
+
+char current_date[15];
+char current_time[15];
+
+char current_hour[3];
+char current_minute[3];
+char current_week_day[10];
 
 // Setup function:
 void setup() {
@@ -10,4 +28,34 @@ void setup() {
     pinMode(mq_pin, INPUT);
 
     Serial.begin(115200);
+
+    configTime(gmt_offset_sec, daylight_offset_sec, NTP_SERVER);
+}
+
+// Loop function:
+void loop() {
+    unsigned long current_millis = millis();
+    timeCapture();
+}
+
+// Time management function:
+void timeCapture() {
+    struct tm time_info;
+
+    if (!getLocalTime(&time_info)) {
+        if (debug >= 1) {
+            Serial.println("- Failed to capture NTP server data.");
+        }
+
+        configTime(gmt_offset_sec, daylight_offset_sec, NTP_SERVER);
+        
+        return;
+    }
+
+    strftime(current_date, 15, "%e/%m/%Y", &time_info);
+    strftime(current_time, 15, "%H:%M:%S", &time_info);
+
+    strftime(current_hour, 3, "%H", &time_info);
+    strftime(current_minute, 3, "%M", &time_info);
+    strftime(current_week_day, 10, "%A", &time_info);
 }
