bbdb12017708f83113722e897e321173d2b8af52
Lucas Ribeiro <lucas.and.rib@gmail.com>
2024-01-05 18:41:34 -0300

    Handles secure Wi-Fi connection.

--- f5-load-balancer-monitor.cpp
+++ f5-load-balancer-monitor.cpp
@@ -1,4 +1,5 @@
 // Libraries import:
+#include <WiFi.h>
 #include <time.h>
 
 // Pinout:
@@ -10,6 +11,10 @@ const int dht_pin = 4;
 #define NTP_SERVER "pool.ntp.br"
 const int daylight_offset_sec = 0;
 const long gmt_offset_sec = -3600*3;
+
+// Wi-fi connection, network SSID and password:
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
 
 // Global variables:
 const int debug = 2;
@@ -30,12 +35,21 @@ void setup() {
     Serial.begin(115200);
 
     configTime(gmt_offset_sec, daylight_offset_sec, NTP_SERVER);
+    connectToWifi();
 }
 
 // Loop function:
 void loop() {
     unsigned long current_millis = millis();
-    timeCapture();
+
+    if (WiFi.status() == WL_CONNECTED) {
+        timeCapture();
+    } else {
+        if (debug >= 1) {
+            Serial.println("- Loss of Wi-Fi connection.");
+        }
+        connectToWifi();
+    }
 }
 
 // Time management function:
@@ -58,4 +72,34 @@ void timeCapture() {
     strftime(current_hour, 3, "%H", &time_info);
     strftime(current_minute, 3, "%M", &time_info);
     strftime(current_week_day, 10, "%A", &time_info);
+}
+
+// Wi-fi connection function:
+void connectToWifi() {
+    if (debug >= 1) {
+        Serial.print("- Connecting to the Wi-Fi network...");
+    }
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    unsigned long wifi_millis = millis();
+
+    while (WiFi.status() != WL_CONNECTED && millis() - wifi_millis < 5000) {
+        if (debug >= 1) {
+            Serial.print(".");
+        }
+        
+        delay(500);
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+        if (debug >= 1) {
+            Serial.print("\n- Connected to Wi-Fi network, IP address: ");
+            Serial.println(WiFi.localIP());
+        }
+    } else {
+        if (debug >= 1) {
+            Serial.println("\n- Failed to connect to Wi-Fi.");
+        }
+    }
 }
